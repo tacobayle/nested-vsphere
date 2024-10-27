f09ad098aa190c243534556ba16c594208b9feae
tacobayle <nicolas.nayle@gmail.com>
2024-10-27 15:29:41 +0100

    20241027_nsx_avi

--- apply.sh
+++ apply.sh
@@ -91,7 +91,7 @@ if [[ ${operation} == "apply" ]] ; then
         -e "s/\${forwarders_bind}/${forwarders_bind}/" \
         -e "s/\${domain}/${domain}/g" \
         -e "s/\${kind}/${kind}/g" \
-        -e "s@\${net_client_list}@${net_client_list}@g" \
+        -e "s@\${net_client_list}@$(echo ${net_client_list} | jq -c -r .)@g" \
         -e "s@\${jsonFile}@$(basename ${jsonFile})@g" \
         -e "s/\${reverse_mgmt}/${reverse_mgmt}/g" \
         -e "s/\${cidr_mgmt_three_octets}/${cidr_mgmt_three_octets}/g" \
