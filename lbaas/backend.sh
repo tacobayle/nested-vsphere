49c9a00e80e75575e4135e2448ae0b71bf738a05
tacobayle <nicolas.nayle@gmail.com>
2024-10-28 08:24:26 +0100

    20241028_nsx_avi

--- lbaas/backend.sh
+++ lbaas/backend.sh
@@ -30,12 +30,12 @@ if [[ ${operation} == "apply" ]] ; then
     app_profile=$(jq -c -r .app_profile $jsonFile)
     if [[ ${app_profile} != "public" && ${app_profile} != "private" ]] ; then echo "ERROR: Unsupported app_profile" ; exit 255 ; fi
     if [[ ${app_profile} == "public" ]] ; then
-      tier1=$(echo ${segments_overlay} | jq -r -c '.[] | select(.lbaas_public == "true").tier1')
-      lbaas_segment=$(echo ${segments_overlay} | jq -r -c --arg arg1 "${tier1}" '.[] | select(.backend == "true" and .tier1 == $arg1).display_name')
+      tier1=$(echo ${segments_overlay} | jq -r -c '.[] | select(.lbaas_public == true).tier1')
+      lbaas_segment=$(echo ${segments_overlay} | jq -r -c --arg arg1 "${tier1}" '.[] | select(.backend == true and .tier1 == $arg1).display_name')
     fi
     if [[ ${app_profile} == "private" ]] ; then
-      tier1=$(echo ${segments_overlay} | jq -r -c '.[] | select(.lbaas_private == "true").tier1')
-      lbaas_segment=$(echo ${segments_overlay} | jq -r -c --arg arg1 "${tier1}" '.[] | select(.backend == "true" and .tier1 == $arg1).display_name')
+      tier1=$(echo ${segments_overlay} | jq -r -c '.[] | select(.lbaas_private == true).tier1')
+      lbaas_segment=$(echo ${segments_overlay} | jq -r -c --arg arg1 "${tier1}" '.[] | select(.backend == true and .tier1 == $arg1).display_name')
     fi
     for backend in $(seq 1 ${count})
     do
